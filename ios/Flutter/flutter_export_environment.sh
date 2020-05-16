1a1df4f683a6ae2f131d92ad493e7e72579ebbc8
Htin Linn Zaw <htinlinnzaw.dev@gmail.com>
2020-05-16 14:18:28 +0630

    First

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/media/root/ad5e01ce-e915-406e-b7d8-c911e13a3cd4/flutter/sdk/development"
+export "FLUTTER_APPLICATION_PATH=/media/root/ad5e01ce-e915-406e-b7d8-c911e13a3cd4/flutter/project/Restaurant-Finder-1/Restaurant-Finder/restaurant-finder-final"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/media/root/ad5e01ce-e915-406e-b7d8-c911e13a3cd4/flutter/sdk/development/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
